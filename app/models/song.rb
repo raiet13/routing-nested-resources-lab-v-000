727a7459cf64d5441846616c6addf9870547e3ad
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 19:46:32 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,3 +10,14 @@ class Song < ActiveRecord::Base
     self.artist = artist
   end
 end
+
+# rspec spec/models/song_spec.rb
+# rspec spec/controllers/songs_controller_spec.rb
+# rspec spec/features/songs_spec.rb
+# rspec spec/views/songs/index.html.erb_spec.rb
+# rspec spec/views/songs/show.html.erb_spec.rb
+
+# rspec spec/controllers/artists_controller_spec.rb
+# rspec spec/features/artists_spec.rb
+# rspec spec/helpers/artists_helper_spec.rb
+# rspec spec/routing/artists_routing_spec.rb
